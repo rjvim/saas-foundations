7c0d2e4480d0659eeef8ab5d27bddc89096f84d2
Rajiv Seelam <rajiv@betalectic.com>
2025-03-12 16:24:32 +0530

    feat: add blog functionality with dynamic routing and metadata generation

--- apps/web/source.config.ts
+++ apps/web/source.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig, defineDocs } from "fumadocs-mdx/config";
+import {
+  defineCollections,
+  defineConfig,
+  defineDocs,
+  frontmatterSchema,
+} from "fumadocs-mdx/config";
+import { z } from "zod";
 
 export const docs = defineDocs({
   dir: "../../content/docs",
+});
+
+export const blogPosts = defineCollections({
+  type: "doc",
+  dir: "../../content/blog",
+  // add required frontmatter properties
+  schema: frontmatterSchema.extend({
+    author: z.string(),
+    date: z.string().date().or(z.date()),
+  }),
 });
 
 export default defineConfig({
