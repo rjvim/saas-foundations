754032378a878f102b80d9c5aab017528f6c3194
Rajiv Seelam <rajiv@betalectic.com>
2025-03-21 16:32:25 +0530

    grunt: add blog

---
+++ apps/oss-dynamic/app/(marketing)/blog/page.tsx
@@ -0,0 +1,3 @@
+import List from "@foundations/cms/blog/list";
+
+export default List;
--- foundations/cms/source.config.ts
+++ foundations/cms/source.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig, defineDocs } from "fumadocs-mdx/config";
+import {
+  defineCollections,
+  defineConfig,
+  defineDocs,
+  frontmatterSchema,
+} from "fumadocs-mdx/config";
+import { z } from "zod";
 
 export const docs = defineDocs({
   dir: "../../workspace/content/docs",
+});
+
+export const blogPosts = defineCollections({
+  type: "doc",
+  dir: "../../workspace/content/blog",
+  schema: frontmatterSchema.extend({
+    author: z.string(),
+    date: z.string().date().or(z.date()),
+  }),
 });
 
 export default defineConfig({
