0c82e3658917b297b3bde036e14d725cc10ac103
Rajiv Seelam <rajiv@betalectic.com>
2025-03-26 11:01:31 +0530

    grunt: add toc to blog

--- foundations/cms/src/blog/single.tsx
+++ foundations/cms/src/blog/single.tsx
@@ -1,6 +1,7 @@
 import { notFound } from "next/navigation";
 import Link from "next/link";
 import { InlineTOC } from "fumadocs-ui/components/inline-toc";
+import { Toc, TOCItems } from "fumadocs-ui/components/layout/toc";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import { blog } from "@foundations/cms/source";
 
@@ -14,30 +15,34 @@ export default async function Page(props: {
   const Mdx = page.data.body;
 
   return (
-    <div className="max-w-7xl mx-auto">
-      <div className="container rounded-xl border py-12">
+    <div className="relative flex flex-col items-center justify-center w-full mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-0 pt-0 bg-red-200">
+      <div className="rounded-xl border py-12">
         <h1 className="mb-2 text-3xl font-bold">{page.data.title}</h1>
         <p className="mb-4 text-fd-muted-foreground">{page.data.description}</p>
         <Link href="/blog">Back</Link>
       </div>
-      <article className="container flex flex-colpy-8">
-        <div className="prose min-w-0">
-          <InlineTOC items={page.data.toc} />
-          <Mdx components={defaultMdxComponents} />
-        </div>
-        <div className="flex flex-col gap-4 text-sm">
-          <div>
-            <p className="mb-1 text-fd-muted-foreground">Written by</p>
-            <p className="font-medium">{page.data.author}</p>
+
+      <div className="flex flex-row gap-4">
+        <TOCItems items={page.data.toc} />
+
+        <article className="flex flex-col py-8 bg-blue-50">
+          <div className="prose min-w-0">
+            <Mdx components={defaultMdxComponents} />
           </div>
-          <div>
-            <p className="mb-1 text-sm text-fd-muted-foreground">At</p>
-            <p className="font-medium">
-              {new Date(page.data.date).toDateString()}
-            </p>
+          <div className="flex flex-col gap-4 text-sm">
+            <div>
+              <p className="mb-1 text-fd-muted-foreground">Written by</p>
+              <p className="font-medium">{page.data.author}</p>
+            </div>
+            <div>
+              <p className="mb-1 text-sm text-fd-muted-foreground">At</p>
+              <p className="font-medium">
+                {new Date(page.data.date).toDateString()}
+              </p>
+            </div>
           </div>
-        </div>
-      </article>
+        </article>
+      </div>
     </div>
   );
 }
