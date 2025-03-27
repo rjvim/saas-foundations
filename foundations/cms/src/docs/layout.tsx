7f32b2598533ddae13997c835a9717530b588c95
Rajiv Seelam <rajiv@betalectic.com>
2025-03-27 14:54:42 +0530

    Add config package

--- foundations/cms/src/docs/layout.tsx
+++ foundations/cms/src/docs/layout.tsx
@@ -1,6 +1,6 @@
 import { DocsLayout } from "fumadocs-ui/layouts/docs";
 import type { ReactNode } from "react";
-import { baseOptions } from "../layout.config";
+import { baseOptions } from "@workspace/config/layout.config";
 import { docsSource } from "@foundations/cms/source";
 
 export default function Layout({ children }: { children: ReactNode }) {
---
+++ workspace/config/src/metadata.ts
@@ -0,0 +1,36 @@
+import type { Metadata } from "next/types";
+
+export function createMetadata(override: Metadata): Metadata {
+  return {
+    ...override,
+    openGraph: {
+      title: override.title ?? undefined,
+      description: override.description ?? undefined,
+      url: "https://saas-foundations-oss-static.vercel.app",
+      images: "/banner.png",
+      siteName: "OSS",
+      ...override.openGraph,
+    },
+    twitter: {
+      card: "summary_large_image",
+      creator: "@rjv_im",
+      title: override.title ?? undefined,
+      description: override.description ?? undefined,
+      images: "/banner.png",
+      ...override.twitter,
+    },
+    alternates: {
+      canonical: "/",
+      types: {
+        "application/rss+xml": "/api/rss.xml",
+      },
+      ...override.alternates,
+    },
+  };
+}
+
+export const baseUrl =
+  process.env.NODE_ENV === "development" ||
+  !process.env.VERCEL_PROJECT_PRODUCTION_URL
+    ? new URL("http://localhost:3000")
+    : new URL(`https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`);
